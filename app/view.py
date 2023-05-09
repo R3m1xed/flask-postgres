b8bf27762dda2592e5dcddcdc7831731af639096
R3m1xed <80881749+R3m1xed@users.noreply.github.com>
2023-05-09 20:41:38 +1000

    Update view.py

--- app/view.py
+++ app/view.py
@@ -3,24 +3,23 @@ import os
 import psycopg2
 import ssl
 
-
+#Make sure to adjust the password to pass you wish.
 conn = psycopg2.connect(database="mydatabase",
                     host="flaskdb",
                     user="postgres",
-                    password="mysecretpassword",
+                    password="MYDATABASEPASSWORD",
                     port="5432")
 
 app = Flask(__name__)
 
+#To enable HTTPS for your app
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain('./server.crt', './server.key')
 
 
 @app.route('/')
 def home():
-
-
-
+  
     cur = conn.cursor()
     cur.execute("SELECT * FROM person")
 
@@ -32,4 +31,5 @@ def home():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
+    #Remove ssl_context to remove https, or keep it if you wish to enable https
     app.run(debug=True, host='0.0.0.0', port=port, ssl_context=context)
